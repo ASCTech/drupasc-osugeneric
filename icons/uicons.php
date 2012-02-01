ed900afbe1f1d64aa71b145af6a46a903890f624
Mike Butsko <butsko.7@osu.edu>
2012-02-01 12:45:54 -0500

    Fixing paths again.

--- icons/uicons.php
+++ icons/uicons.php
@@ -1,18 +1,20 @@
 <p>
 	If you are having trouble accessing this site and need to<br />
-	request an alternate format, please contact: <a href="http://artsandsciences.osu.edu/contact">Web Manager</a>
+	request an alternate format, please contact: <a href="http://artsandsciences.osu.edu/contact/contact.cfm">Web Manager</a>
 </p>
 <p>
 
+<?php $theme_path = base_path() . path_to_theme(); ?>
+
 <?php if(user_is_logged_in()) { ?>
-	<a href="http://jigsaw.w3.org/css-validator/check/referer" style="margin-right: 5px; text-decoration: none; border-bottom: none;"><img src="w3c_css.png" height="13px" title="Validate CSS" alt="Validate CSS" /></a>
-	<a href="http://validator.w3.org/check?uri=referer" style="margin-right: 5px; text-decoration: none; border-bottom: none;"><img src="/sites/all/themes/osugeneric/icons/w3c_xhtml.png" height="13px" title="Validate XHTML" alt="Validate XHTML" /></a>
+  <a href="http://jigsaw.w3.org/css-validator/check/referer" style="margin-right: 5px; text-decoration: none; border-bottom: none;"><img src="<?php print $theme_path; ?>/icons/w3c_css.png" height="13px" title="Validate CSS" alt="Validate CSS" /></a>
+	<a href="http://validator.w3.org/check?uri=referer" style="margin-right: 5px; text-decoration: none; border-bottom: none;"><img src="<?php print $theme_path; ?>/icons/w3c_xhtml.png" height="13px" title="Validate XHTML" alt="Validate XHTML" /></a>
 <?php } ?>
 
 <?php if(!user_is_logged_in()) { ?>
-	<a href="<?=base_path();?>user/login?<?=drupal_get_destination();?>" style="text-decoration: none; border-bottom: none;"><img src="/sites/all/themes/osugeneric/icons/user.gif" height="13px" title="Site Administrator Login" alt="Site Administrator Login" /></a>
+	<a href="<?php base_path();?>user/login?<?=drupal_get_destination();?>" style="text-decoration: none; border-bottom: none;"><img src="<?php print $theme_path; ?>/icons/user.gif" height="13px" title="Site Administrator Login" alt="Site Administrator Login" /></a>
 <?php } else { ?>
-	<a href="<?=base_path();?>logout?<?=drupal_get_destination();?>" style="text-decoration: none; border-bottom: none;"><img src="/sites/all/themes/osugeneric/icons/user_out.gif" height="13px" title="Logout" alt="Logout" /></a>
+	<a href="<?php base_path();?>logout?<?=drupal_get_destination();?>" style="text-decoration: none; border-bottom: none;"><img src="<?php print $theme_path; ?>/icons/user_out.gif" height="13px" title="Logout" alt="Logout" /></a>
 <?php } ?>
 </p>
 
